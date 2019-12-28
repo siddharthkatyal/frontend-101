38f8d1bc43e34399b9842b50ff19bb1a120ca457
siddharthkatyal <54847921+siddharthkatyal@users.noreply.github.com>
2019-12-28 20:17:34 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,4 @@
+if(document.URL=="https://www.bits-bosm.org/" || document.URL=="https://www.bits-oasis.org/" || document.URL == "https://www.bits-apogee.org/")
+{
+    console.log("welcome to dvm website");
+}
