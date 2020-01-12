8c75e7fed09c7da9f23ec5f0a233238c024ff999
Siddharth Katyal <54847921+siddharthkatyal@users.noreply.github.com>
2020-01-12 20:17:14 +0530

    Delete content.js

--- content.js
+++
@@ -1,4 +0,0 @@
-if(document.URL=="https://www.bits-bosm.org/" || document.URL=="https://www.bits-oasis.org/" || document.URL == "https://www.bits-apogee.org/")
-{
-    console.log("welcome to dvm website");
-}
